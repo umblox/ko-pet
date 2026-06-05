3ffc913360bd4d24fa3abea1b3ddcbaf9a8a4b36
umblox <39423646+umblox@users.noreply.github.com>
2026-06-05 23:38:46 +0700

    Update PetViewModel.kt

--- app/src/main/java/com/aipet/app/ui/PetViewModel.kt
+++ app/src/main/java/com/aipet/app/ui/PetViewModel.kt
@@ -4,14 +4,24 @@ import androidx.lifecycle.ViewModel
 import kotlinx.flow.MutableStateFlow
 import kotlinx.flow.StateFlow
 
-enum class PetState { IDLE, DETECTED, TALKING }
-
-class PetViewModel : ViewModel() {
-    private val _petState = MutableStateFlow(PetState.IDLE)
-    val petState: StateFlow<PetState> = _petState
-
-    fun updateState(newState: PetState) {
-        _petState.value = newState
-    }
+enum class PetEmotion {
+    IDLE,       // Standby berkedip normal
+    HAPPY,      // Tersenyum lebar (Mata melengkung ke atas)
+    SAD,        // Sedih (Mata sayu melengkung ke bawah)
+    ANGRY,      // Marah (Mata miring tajam ke dalam)
+    SURPRISED,   // Kaget/Bingung melihat orang tidak dikenal (Mata bulat besar)
+    THINKING,   // Berpikir/Proses mengenali (Mata menyipit)
+    SLEEPY,     // Mengantuk (Mata setengah tertutup)
+    WINK,       // Berkedip sebelah (Genit/Bercanda)
+    BORED,      // Bosan dicuekin (Mata berbentuk garis horizontal datar)
+    LOADING     // Sedang memproses database
 }
 
+class PetViewModel : ViewModel() {
+    private val _emotion = MutableStateFlow(PetEmotion.IDLE)
+    val emotion: StateFlow<PetEmotion> = _emotion
+
+    fun setEmotion(newEmotion: PetEmotion) {
+        _emotion.value = newEmotion
+    }
+}
